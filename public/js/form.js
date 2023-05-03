c4f19922f0f489c3dd04d701bd670490e1882152
dgichaba <dorothygichaba@gmail.com>
2023-05-03 17:58:35 +0300

    add files on repo

---
+++ public/js/form.js
@@ -0,0 +1,13 @@
+function sendMessage(){
+  var name =$("#name").val();
+  var email=$("#email").val();
+  var message=$("#message").val();
+  if (name === "" || email==="" || message===""){
+    $("#response").html("");
+    
+ }else{
+  $("#error").html("");
+  $("#response").focus();
+  $("#response").html("Thank you " +name+"! Your message has been received. We shall get back to you shorty");
+ }
+}
